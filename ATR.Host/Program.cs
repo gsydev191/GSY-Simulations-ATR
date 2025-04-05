c8684bf75ccf70d7a64e688d987daeff56a48fd6
Dan <gsygaming@gmail.com>
2025-04-05 12:40:58 +0100

    Basic working dx11

--- ATR.Host/Program.cs
+++ ATR.Host/Program.cs
@@ -9,6 +9,9 @@ using Silk.NET.Input.Glfw;
 using Silk.NET.DXGI;
 using Silk.NET.Direct3D.Compilers;
 using Silk.NET.Maths;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Runtime.CompilerServices;
+using System.Drawing;
 
 namespace ATR.Host
 {
@@ -26,6 +29,8 @@ namespace ATR.Host
         private static ComPtr<ID3D11DeviceContext> _context = default;
         private static ComPtr<ID2D1Factory1> _d2dFactory = default;
 
+        private static float[] _backgroundColor = [0.392f, 0.584f, 0.929f, 1.0f];
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -40,7 +45,7 @@ namespace ATR.Host
                 API = GraphicsAPI.None
             };
 
-            _window = Window.Create(options);
+            _window = Silk.NET.Windowing.Window.Create(options);
 
             _window.Load += OnLoad;
             _window.Update += OnUpdate;
@@ -63,8 +68,10 @@ namespace ATR.Host
 
         }
 
-        private static void OnLoad()
+        private static unsafe void OnLoad()
         {
+            _window.Center();
+
             const bool forceDxvk = false;
             _dxgi = DXGI.GetApi(_window, forceDxvk);
             _d3d11 = D3D11.GetApi(_window, forceDxvk);
@@ -76,12 +83,55 @@ namespace ATR.Host
                 keyboard.KeyDown += KeyDown;
             }
 
+            SilkMarshal.ThrowHResult
+            (
+                _d3d11.CreateDevice
+                (
+                    default(ComPtr<IDXGIAdapter>),
+                    D3DDriverType.Hardware,
+                    Software: default,
+                    (uint)CreateDeviceFlag.Debug,
+                    null,
+                    0,
+                    D3D11.SdkVersion,
+                    ref _device,
+                    null, 
+                    ref _context
+                    )
+            );
 
+            if (OperatingSystem.IsWindows())
+            {
+                #if DEBUG
+                _device.SetInfoQueueCallback(msg => Console.WriteLine(SilkMarshal.PtrToString((nint)msg.PDescription)));
+                #endif
+            }
 
-            _window.Center();
+            // Create our swapchain.
+            var swapChainDesc = new SwapChainDesc1
+            {
+                BufferCount = 2, // double buffered
+                Format = Format.FormatB8G8R8A8Unorm,
+                BufferUsage = DXGI.UsageRenderTargetOutput,
+                SwapEffect = SwapEffect.FlipDiscard,
+                SampleDesc = new SampleDesc(1, 0)
+            };
 
+            _factory = _dxgi.CreateDXGIFactory<IDXGIFactory2>();
 
-
+            // Create the swapchain.
+            SilkMarshal.ThrowHResult
+            (
+                _factory.CreateSwapChainForHwnd
+                (
+                    _device,
+                    _window.Native!.DXHandle!.Value,
+                    in swapChainDesc,
+                    null,
+                    ref Unsafe.NullRef<IDXGIOutput>(),
+                    ref _swapChain
+                )
+            );
         }
 
         private static void OnUpdate(double deltaTime)
@@ -89,9 +139,29 @@ namespace ATR.Host
 
         }
 
-        private static void OnRender(double deltaTime)
+        private static unsafe void OnRender(double deltaTime)
         {
+            using var framebuffer = _swapChain.GetBuffer<ID3D11Texture2D>(0);
 
+            // Create a view over the render target.
+            ComPtr<ID3D11RenderTargetView> renderTargetView = default;
+            SilkMarshal.ThrowHResult(_device.CreateRenderTargetView(framebuffer, null, ref renderTargetView));
+
+            // Clear the render target to be all black ahead of rendering.
+            _context.ClearRenderTargetView(renderTargetView, ref _backgroundColor[0]);
+
+            // Update the rasterizer state with the current viewport.
+            var viewport = new Viewport(0, 0, _window.FramebufferSize.X, _window.FramebufferSize.Y, 0, 1);
+            _context.RSSetViewports(1, in viewport);
+
+            // Tell the output merger about our render target view.
+            _context.OMSetRenderTargets(1, ref renderTargetView, ref Unsafe.NullRef<ID3D11DepthStencilView>());
+
+            // Present the drawn image.
+            _swapChain.Present(1, 0);
+
+            // Clean up any resources created in this method.
+            renderTargetView.Dispose();
         }
 
         private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
